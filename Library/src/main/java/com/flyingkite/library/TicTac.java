8ee4abfcf3832e8800b8b8e1650742115172eb95
chenFlyingkite <30795482+chenFlyingkite@users.noreply.github.com>
2017-11-30 19:46:43 +0800

    Make the tictac's to be silentable, and add the usage in JavaDoc

--- Library/src/main/java/com/flyingkite/library/TicTac.java
+++ Library/src/main/java/com/flyingkite/library/TicTac.java
@@ -4,10 +4,60 @@ import android.util.Log;
 
 import java.util.Stack;
 
+/**
+ * The simple class for performance profiling.
+ * It uses a static Stack to hold all the time stamps where {@link #tic()} is called.
+ * For tracking performance between multiple classes or other purposes
+ * (Like tracking performance in different {@link android.os.AsyncTask})
+ * , consider to use {@link TicTac2} to correctly profiling.
+ *
+ * <p>The naming idea from TicTac is from Matlab's keywords : tic tac</p>
+ *
+ * <p>Here is an example of usage:</p>
+ * <pre class="prettyprint">
+ * public class Main {
+ *     public static void main(String[] args) {
+ *         // Let's start the tic-tac
+ *         TicTac.tic();
+ *             f();
+ *         TicTac.tac("f is done");
+ *         TicTac.tic();
+ *             g();
+ *             TicTac.tic();
+ *                 g1();
+ *             TicTac.tac("g1 is done");
+ *             TicTac.tic();
+ *                 g2();
+ *             TicTac.tac("g2 is done");
+ *         TicTac.tac("g + g1 + g2 is done");
+ *         // Now is ended
+ *     }
+ *
+ *     private void f() {
+ *         // your method body
+ *     }
+ *     private void g() {
+ *          // your method body
+ *     }
+ *     private void g1() {
+ *          // your method body
+ *     }
+ *     private void g2() {
+ *          // your method body
+ *     }
+ * }
+ * </pre>
+ */
 public class TicTac {
-    private static final String TAG = "Hi";
+    private static final String TAG = "TicTac";
     // A handy tic-tac to track the performance
     private static final Stack<Long> tictac = new Stack<>();
+
+    public static boolean showLog = true;
+
+    public static void reset() {
+        tictac.clear();
+    }
 
     public static void tic() {
         tictac.push(System.currentTimeMillis());
@@ -37,11 +87,15 @@ public class TicTac {
         log(s.toString());
     }
 
-    private static void logError(long tac, String msg) {
-        Log.e(TAG, "X_X [tic = N/A, tac = " + tac + "] : " + msg);
+    protected static void logError(long tac, String msg) {
+        if (showLog) {
+            Log.e(TAG, "X_X [tic = N/A, tac = " + tac + "] : " + msg);
+        }
     }
 
-    private static void log(String s) {
-        Log.e(TAG, s);
+    protected static void log(String s) {
+        if (showLog) {
+            Log.e(TAG, s);
+        }
     }
 }
--- Library/src/main/java/com/flyingkite/library/TicTac2.java
+++ Library/src/main/java/com/flyingkite/library/TicTac2.java
@@ -4,16 +4,54 @@ import android.util.Log;
 
 import java.util.Stack;
 
-/** The class performing the same intention with {@link TicTac}.
+/**
+ * The class performing the same intention with {@link TicTac}.
  * Unlike {@link TicTac} provides static method and uses global time stack,
  * {@link TicTac2} provides for creating instance and use its own one time stack. <br/>
  * {@link TicTac2} is specially better usage for tracking performance in different AsyncTasks,
  * by each task create a new object and call its {@link TicTac2#tic()} and {@link TicTac2#tac(String)} in task.
+ *
+ * <p>Here is an example of usage:</p>
+ * <pre class="prettyprint">
+ * public class Main {
+ *     public static void main(String[] args) {
+ *         // Let's start the tic-tac
+ *         TicTac.tic();
+ *             f();
+ *         TicTac.tac("f is done");
+ *         TicTac.tic();
+ *             g();
+ *             TicTac.tic();
+ *                 g1();
+ *             TicTac.tac("g1 is done");
+ *             TicTac.tic();
+ *                 g2();
+ *             TicTac.tac("g2 is done");
+ *         TicTac.tac("g + g1 + g2 is done");
+ *         // Now is ended
+ *     }
+ *
+ *     private void f() {
+ *         // your method body
+ *     }
+ *     private void g() {
+ *          // your method body
+ *     }
+ *     private void g1() {
+ *          // your method body
+ *     }
+ *     private void g2() {
+ *          // your method body
+ *     }
+ * }
+ * </pre>
  */
 public class TicTac2 {
-    private static final String TAG = "Hi2";
+    private static final String TAG = "TicTac2";
     // A handy tic-tac to track the performance
     private final Stack<Long> tictac = new Stack<>();
+
+    public boolean showLog = true;
 
     public void tic() {
         tictac.push(System.currentTimeMillis());
@@ -48,11 +86,19 @@ public class TicTac2 {
     }
 
     protected void logError(long tac, String msg) {
-        Log.e(TAG, "X_X [tic = N/A, tac = " + tac + "] : " + msg);
+        if (showLog) {
+            Log.e(TAG, "X_X [tic = N/A, tac = " + tac + "] : " + msg);
+        }
     }
 
     protected void logTac(String s) {
-        Log.e(TAG, s);
+        if (showLog) {
+            Log.e(TAG, s);
+        }
     }
 
+    @Override
+    public String toString() {
+        return String.format("%s : tictac.size() = %s", TAG, tictac.size());
+    }
 }
