d4fbc9131f01923d9cae95db24d416dccebf874e
chenFlyingkite <30795482+chenFlyingkite@users.noreply.github.com>
2018-05-10 19:20:32 +0800

    Add flexible thread pool, it is  fixed thread pools but it can shutdown to 0 size

--- Library/src/main/java/com/flyingkite/library/ThreadUtil.java
+++ Library/src/main/java/com/flyingkite/library/ThreadUtil.java
@@ -6,13 +6,33 @@ import android.util.Log;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public final class ThreadUtil {
     private ThreadUtil() {}
 
     //-------------------------------------------------------------------------
-    // The thread pool for classes that have many simple tasks to be run
+    /**
+     * The thread pool for classes that have many simple tasks to be run
+     */
     public static final ExecutorService cachedThreadPool = Executors.newCachedThreadPool();
+    //-------------------------------------------------------------------------
+
+    //-------------------------------------------------------------------------
+    /**
+     * @return {@link #newFlexThreadPool(int, long)} with alive time = 60 sec
+     */
+    public static ExecutorService newFlexThreadPool(int atMost) {
+        return newFlexThreadPool(atMost, 60);
+    }
+    /**
+     * Creates thread pool similar with {@link Executors#newFixedThreadPool(int)}, with atMost & alive time provided
+     */
+    public static ExecutorService newFlexThreadPool(int atMost, long aliveSecond) {
+        return new ThreadPoolExecutor(0, atMost, aliveSecond, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>());
+    }
     //-------------------------------------------------------------------------
 
     public static boolean isUIThread() {
